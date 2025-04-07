125271df0842b1716c7baf54b3f6241b24750499
jgibson18seneca <joseph_1.01@hotmail.com>
2025-04-07 10:21:26 -0400

    Updated database info

--- mysql.sql
+++ mysql.sql
@@ -8,7 +8,7 @@ last_name VARCHAR(20),
 primary_skill VARCHAR(20),
 location VARCHAR(20));
 
-INSERT INTO employee VALUES ('1','Amanda','Williams','Smile','local');
-INSERT INTO employee VALUES ('1','Alan','Williams','Empathy','alien');
+INSERT INTO employee VALUES ('1','Joe','Gibson','Productivity','Local');
+INSERT INTO employee VALUES ('2','Ale','Malcolm','Empathy','Foreign');
 SELECT * FROM employee;
 
