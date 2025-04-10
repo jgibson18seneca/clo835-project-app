bcc7024191ad75e32f65eb8e7ef22300cddcefaf
jgibson18seneca <joseph_1.01@hotmail.com>
2025-04-10 10:42:02 -0400

    Updated app.py to properly call from S3 Bucket

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from pymysql import connections
 import os
+import boto3
 import requests
 import random
 import argparse
@@ -16,20 +17,36 @@ DBPORT = int(os.environ.get("DBPORT"))
 
 # Amazon S3 Bucket URL (replace with your actual S3 URL or image path)
 S3_BUCKET_URL = "https://clo835-project-s3.s3.us-east-1.amazonaws.com/JGibson.jpg" # Check URL after creation
+# S3_BUCKET_URL = 'https://clo835-project-s3.s3.us-east-1.amazonaws.com/JGibson.jpg?response-content-disposition=inline&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEAUaCXVzLWVhc3QtMSJHMEUCIE0xPu0ryRBoMDLC84p%2FQioSbyQvvPJFDAnMZLs3GrLJAiEArGZwpDV6y4qmu6ZoHHggZbuEQ89A8XggGv1T1IPMBrwq2gMIfRABGgwxMzQ5ODY4MDA5MzgiDCq2qn8nX8sVoGcWTiq3AyNEZp7hduVJ%2BMPCTN4%2F%2FTEg%2FBJJGmxCEO0m8HOlu3stPLUzGlI%2B5jl3NrbuJj1c5k3ImLea%2BPsd4Y%2FSCla3CmRkKKys8aOrdVwZYBVFHyGx%2Bm65Xk10l%2F5EuPDWo8LNU5uFQ6qsfK5dXWM8og1MtuhEZnVpttKKpHqyLclRt32b0UcbZfKcPVoQLAfheH941x6qgUgBNh%2FWO33cowhBqYOkgLElz9sQFfruWHlDnZU0MddRcVdDf%2FudW0zwNPjqkzRjozA66gz4orZsG%2FaTlcxVdCVe6XEalaghzzBxULnQ1pG8l%2Bhm8uJJX9HkZDuGlajdTifUqaYe%2Fx2ke1chQvMneTfG0nG8%2Fsu0Z9YgEtYm3zJnhk1dOcugc8%2Bvb8fHHj4qSId%2F2IYIZwDSaknx7UJdOaPuzZqNcmNzHCFaUEuVtIV9GyEPWcjsSLQTMAspjbbc9KxfPZk1vJQsy7K1c6Ok%2B4cWSmiwbYOuxaYI0mVENvPpFYhui%2BLqjc%2BADWuadvivJv2Q9%2FBG1V0KJYqb0%2FgDNEyBgozrYeS%2FwFKamTQXt3tPNyyIPH4M6Txlkvz4phx3vdUNuEEwg%2FHVvwY6twKtJoEsMC5sI%2FZFx%2FVhhpv%2Fsq3RUDs4ohE6l9w5vglSaKTiOlFHRKVAe9BN%2BoblNvWMmR2dCUHu6Mx1yyhMyaLmQtIH39gCsg%2B%2FcU9GZHcWl6fDB0tC3n7Dt149AcDSE2Givz1aZl1zGXwrY8fwmGEBr6LUGOSx8tB89ZVXWJQw0wAwPwHZCCTNyNK3cNmd3uFJ%2BgrcpgDYDcXupz%2B0hIqiNvhqjTUtGHljskuc4%2FOVFxEhBmDsVc%2FYXPN2aoO0J8JDTCWEvYwD%2FaWpGeVS5TiGWPl3mwSTNdXsnRcr3PpOpWXOWhWJWhYO71sBd%2FLGM1WedmcyTg2ckRYxu%2FaCjWPELYHB08DH44tG4kAiOALTvQMWKaGBXOXvScxHOxxjJ84dJ1iz%2BanGVIIOg8wsYDhCNtA1%2BFDQvQ%3D%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ASIAR63OXD4VLNRTGNDY%2F20250408%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250408T200643Z&X-Amz-Expires=43200&X-Amz-SignedHeaders=host&X-Amz-Signature=dfee176fb7af830078eae4465ae04c4f0d18f4d16046334e29cc84ca201f00ff'
+
+# s3 = boto3.client('s3')
+
+# bucket_name = 'clo835-project-s3'
+# object_key = 'JGibson.jpg'
+
+# response = s3.get_object(Bucket=bucket_name, Key=object_key)
+
+# image_data = response['Body'].read()
+
+# with open('downloaded_test.jpg', 'wb') as f:
+#     f.write(image_data)
+
+# print("Image downloaded successfully!")
 
 # TEST CODE
 
 # Retrieve the S3 URL from the environment variable
-s3_image_url = os.getenv('S3_BUCKET_URL')
+# s3_image_url = os.getenv('S3_BUCKET_URL')
+# s3_image_url = S3_BUCKET_URL
 
-# Example: Fetch the image from S3 using requests
-response = requests.get(s3_image_url)
+# # Example: Fetch the image from S3 using requests
+# response = requests.get(s3_image_url)
 
-if response.status_code == 200:
-    with open('JGibson.jpg', 'wb') as f:
-        f.write(response.content)
-else:
-    print(f"Failed to fetch image: {response.status_code}")
+# if response.status_code == 200:
+#     with open('JGibson.jpg', 'wb') as f:
+#         f.write(response.content)
+# else:
+#     print(f"Failed to fetch image: {response.status_code}")
 
 # TEST CODE END
 
